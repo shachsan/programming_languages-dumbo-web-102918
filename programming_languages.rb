3890a34dbaff3e6326fbecb1fb3cb40ace38f5e4
SANJAY K SAI <shachsan@hotmail.com>
2018-10-15 19:53:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,41 @@
 def reformat_languages(languages)
-  
+  languages.each do |style, name_type|
+    language = name_type.keys
+  end
+  p language 
 end
+
+reformat_languages
+
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
