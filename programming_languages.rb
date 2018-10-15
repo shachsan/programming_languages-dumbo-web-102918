e2fc7a5c228615dcc33c07b7a066e6b1a9025253
SANJAY K SAI <shachsan@hotmail.com>
2018-10-15 20:38:36 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,11 +32,29 @@ languages = {
 
 
 def reformat_languages(languages)
-  language=[]
+  progmming_languages=[]
+  reformed_hash = {}
   languages.each do |style, name_type|
-    language += name_type.keys
+    progmming_languages += name_type.keys
+
+
+    end
   end
-  p language
+
+  ## need to create new hash with language as key to another hash as value that contains
+  #style and type. Eg. {Ruby=>{style=>[], type => ""}}
+  progmming_languages.uniq.each do |language|
+    languages.each do |style, name_type|
+      style_arr = []
+      name_type.each do |lang_name, lang_type|
+        if language == lang_name
+          reformed_hash[language] = {:style => style_arr << style ,lang_type }
+        end
+      end
+    end
+    # reformed_hash[language] = style_type_hash
+  end
+  p reformed_hash
 end
 
 reformat_languages(languages)
