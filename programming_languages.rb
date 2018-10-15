001e188297dbd3494ef6a9e7f0ddcc58cb26f89e
SANJAY K SAI <shachsan@hotmail.com>
2018-10-15 19:54:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,7 +34,7 @@ languages = {
 def reformat_languages(languages)
   language=[]
   languages.each do |style, name_type|
-    language = name_type.keys
+    language << name_type.keys
   end
   p language
 end
