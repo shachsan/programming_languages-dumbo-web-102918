fa3fe7245f589ad8cbc40eec087974643947d0f6
SANJAY K SAI <shachsan@hotmail.com>
2018-10-15 21:09:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,35 +1,3 @@
-# languages = {
-#   :oo => {
-#     :ruby => {
-#       :type => "interpreted"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     },
-#     :python => {
-#       :type => "interpreted"
-#     },
-#     :java => {
-#       :type => "compiled"
-#     }
-#   },
-#   :functional => {
-#     :clojure => {
-#       :type => "compiled"
-#     },
-#     :erlang => {
-#       :type => "compiled"
-#     },
-#     :scala => {
-#       :type => "compiled"
-#     },
-#     :javascript => {
-#       :type => "interpreted"
-#     }
-#
-#   }
-# }
-
 def reformat_languages(languages)
   progmming_languages=[]
   reformed_hash = {}
